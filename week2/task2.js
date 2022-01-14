840e6b49ed65f51c9e8f4f410e98dd76a2ea2b4d
caiwoods11 <caiwoods11@byui.edu>
2022-01-14 13:14:20 -0800

    initial commit

---
+++ week2/task2.js
@@ -0,0 +1,59 @@
+/*Complete the following steps
+In the task2.js file:
+
+1. Declare and instantiate a variable to hold your name*/
+document.getElementById('elementId')
+
+/*2. Place the value of the name variable into the task2.html
+ file inside of the element with an "id of "name" 
+ (hint: document.querySelector())
+
+3. Declare and instantiate a variable to hold the current year
+
+4. Place the value of the current year variable into the task2.html 
+file inside of the element with an id of "year"
+
+5. Copy an image of yourself into the images folder
+
+6. Declare and instantiate a variable to hold the name 
+and location of your picture (hint: images/imagename.png)
+
+7. Place the value of the picture variable into the task2.html 
+file in the "src" attribute of the img element 
+(hint: document.querySelector().setAttribute())
+
+
+Arrays
+
+Complete the following steps
+In the task2.js file:
+
+1. Declare an array variable to hold your favorite foods 
+( hint: [] ) and fill it with your favorite foods as strings.
+
+2. Use JavaScript to modify the element who's id is "food" found 
+in the task2.html file.
+
+3. Declare and instantiate a variable to hold another of your 
+favorite foods.
+
+4. Add the value stored in this variable to the favorite food array.
+
+5. Repeat Step 2
+Remove the first element in the favorite foods array
+
+6. Repeat Step 2
+Remove the last element in the favorite foods array
+
+7. Repeat Step 2
+
+
+Review
+After completing the previous steps:
+
+1. Review one of the many possible solutions for this task,
+
+2. Compare and contrast your code with the possible solution's 
+code, and
+
+3. Make note of any improvements you could make for future tasks.*/
